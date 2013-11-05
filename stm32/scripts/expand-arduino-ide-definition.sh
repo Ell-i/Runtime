1b8f63223d77af8d67040aa1c6a4884e8b0cf7b6
Pekka Nikander <pekka.nikander@senseg.com>
2013-11-05 18:24:41 +0200

    Add a shell script to extract variable values out of Arduino *.txt configuration files

---
+++ stm32/scripts/expand-arduino-ide-definition.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# XXX Rewrite with python or something
+#
+
+RESULT="{$1}"; shift
+FILES="$@"
+
+# Expand as long as there are {variable} constructs in the result
+while echo "$RESULT" | grep -q '{.*}'; do
+    # Extract the variable name from the result
+    VAR=`echo "$RESULT" | sed -e 's/.*{\([^}]*\)}.*/\1/'`
+    # Find the first immediate expansion
+    EXPANSION=`sed -n -e '/^#/d' -e "/$VAR=/{s/$VAR=[ \t]*//p
+q}" $FILES | sed -e 's%/%\\\\/%g'`
+    # echo "$VAR" "->" "$EXPANSION" >&2
+    # Replace the variable with the expansion
+    RESULT=`echo "$RESULT" | sed -e "s/{$VAR}/$EXPANSION/"`
+done
+
+echo $RESULT
