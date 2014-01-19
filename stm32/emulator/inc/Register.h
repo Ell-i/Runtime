099e7655540ccdd0f3d82657100336c0b5706653
Pekka Nikander <pekka.nikander@senseg.com>
2014-01-19 19:18:07 +0200

    Fix copyright messages in emulator

--- stm32/emulator/inc/Register.h
+++ stm32/emulator/inc/Register.h
@@ -1,5 +1,20 @@
 /*
- * Copyright (c) 2013 ELL-i co-operative
+ * Copyright (c) 2013-2014 ELL-i co-operative.
+ *
+ * This is part of ELL-i software.
+ *
+ * ELL-i software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ELL-i software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ELL-i software.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _REGISTER_H_
