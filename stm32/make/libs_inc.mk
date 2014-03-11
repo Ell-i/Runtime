c4df7771e3a8d80744024fa4e2544e9a31a58cc5
Ivan Raul <supra.material@gmail.com>
2014-03-11 13:57:57 +0100

    Add Linking Enc28j60 library

--- stm32/make/libs_inc.mk
+++ stm32/make/libs_inc.mk
@@ -7,6 +7,11 @@
 CFLAGS    += $(patsubst %,-I%,$(wildcard $(TOP)libraries/*/.))
 CXXFLAGS  += $(patsubst %,-I%,$(wildcard $(TOP)libraries/*/.))
 
+CFLAGS    += $(patsubst %,-I%,$(wildcard $(TOP)libraries/CoAP/arch/*/.))
+CXXFLAGS  += $(patsubst %,-I%,$(wildcard $(TOP)libraries/CoAP/arch/*/.))
+
 LIBS      += $(wildcard $(TOP)libraries/*/*.a)
+LIBS      += $(wildcard $(TOP)libraries/CoAP/arch/*/*.a)
 LIBS      += $(TOP)libraries/SPI/ellduino_spi.o
+
 
