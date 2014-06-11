c7b8d8d3052b1b1590045e34f85ba0ea33998f9f
Pekka Nikander <pekka.nikander@senseg.com>
2014-06-11 17:43:23 +0300

    Add PCAP dumping to test_CoAP_net_packet.c

--- stm32/tests/test_CoAP_net_packet/test_CoAP_net_packet.c
+++ stm32/tests/test_CoAP_net_packet/test_CoAP_net_packet.c
@@ -21,7 +21,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <netinet/net_packet.h>
-
+#include <main.h>
 
 const struct net_packet test_arp_packet = {
 };
@@ -61,14 +61,44 @@ struct net_packet test_packet_buffer; // No space for actual payload
 // Mockup for testing
 uint8_t ether_laddr[ETHER_ADDR_LEN] = { 1, 2, 3, 4, 5, 6 };
 
-void eth_output(const void *payload, uint16_t payload_len) {
-    // XXX
+#ifdef EMULATOR
+/* If running in the emulator, output using pcap */
+#include <pcap/pcap.h>
+#include <err.h>
+#include <sysexits.h>
+
+pcap_t *pcap;
+pcap_dumper_t *dumper;
+
+void setup_pcap(void) {
+    pcap = pcap_open_dead(DLT_EN10MB, 512 /* XXX */);
+
+    /* Use stdout as a PCAP dumper for outputting the packet; see Makefile tests: */
+    dumper = pcap_dump_fopen(pcap, stdout);
+    if (NULL == dumper) {
+        err(EX_OSERR,   "Exiting.  Failed to open output for packet dumping.");
+    }
 }
 
+void eth_output(const void *payload, uint16_t payload_len) {
+    struct ether_header *ether =
+        (struct ether_header *)(((char *)payload) - ETHER_HEADER_LEN);
+
+    /* Dump the packet to stdout in the PCAP format */
+    struct pcap_pkthdr pkthdr;
+    gettimeofday(&pkthdr.ts, NULL);
+    pkthdr.caplen = pkthdr.len = sizeof(struct ether_header) + payload_len;
+    pcap_dump((u_char *)dumper, &pkthdr, (const u_char *)ether);
+}
+#endif
+
 void setup() {
+#ifdef EMULATOR
+    setup_pcap();
+#endif
     memcpy(&test_packet_buffer, &test_ip_icmp_packet, sizeof(test_packet_buffer));
     memcpy(test_packet_buffer.np_ether.ether_shost,
-           ether_laddr,a
+           ether_laddr,
            sizeof(test_packet_buffer.np_ether.ether_shost));
     test_packet_buffer.np_ip.ip_id = 12345; // XXX randomize or something
     test_packet_buffer.np_icmp.icmp_sum
