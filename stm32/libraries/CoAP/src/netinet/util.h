da4572be1f6f84f52dbbb290286b95cabf8a4414
Jose Granados <josedgv@yahoo.com>
2014-06-13 17:44:45 +0300

    Fix CoAP library emulator compilation under Linux

--- stm32/libraries/CoAP/src/netinet/util.h
+++ stm32/libraries/CoAP/src/netinet/util.h
@@ -42,6 +42,30 @@ static inline int htonl(uint32_t l) { return __REV(l);   };
 static inline int ntohl(uint32_t l) { return __REV(l);   };
 
 # else //EMULATOR
+#include <endian.h>
+
+/* Get machine dependent optimized versions of byte swapping functions.  */
+#include <bits/byteswap.h>
+
+
+/* We can optimize calls to the conversion functions.  Either nothing has
+   to be done or we are using directly the byte-swapping functions which
+   often can be inlined.  */
+# if __BYTE_ORDER == __BIG_ENDIAN
+/* The host byte order is the same as network byte order,
+   so these functions are all just identity.  */
+# define ntohl(x)       (x)
+# define ntohs(x)       (x)
+# define htonl(x)       (x)
+# define htons(x)       (x)
+# else
+#  if __BYTE_ORDER == __LITTLE_ENDIAN
+#   define ntohl(x)     __bswap_32 (x)
+#   define ntohs(x)     __bswap_16 (x)
+#   define htonl(x)     __bswap_32 (x)
+#   define htons(x)     __bswap_16 (x)
+# endif
+#endif
 
 /* Defined already in the emulator header files, don't redefine here. */
 
