62c299b0242368fcae6a50693a20661e366b5d7a
Pekka Nikander <pekka.nikander@senseg.com>
2014-09-19 10:24:05 +0300

    Add missing variants/stm32f334nucleo/variant.h

---
+++ stm32/variants/stm32f334nucleo/variant.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014 ELL-i co-operative.
+ *
+ * ELL-i software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ELL-i software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ELL-i software.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Authors:  Pekka Nikander <pekka.nikander@ell-i.org>  2014
+ */
+
+#ifndef _VARIANT_H_
+#define _VARIANT_H_
+
+#endif //_VARIANT_H_
